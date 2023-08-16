6de1607f474e494ee7d5e0320e1262238f0b0cb0
Łukasz Cieślak <56347002+xlazure@users.noreply.github.com>
2023-08-16 23:47:27 +0200

    clean up

---
+++ todo-app/src/components/todoForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <form>
+        <input type="text" placeholder="your work" v-model="newTodo"/>
+    </form>
+</template>
+
+<script>
+export default ({
+    name:"TodoForm"
+})
+
+</script>
+
+<style>
+</style>
