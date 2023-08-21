63d77843b97fed1a968f197ea2790e66d0764984
Łukasz Cieślak <56347002+xlazure@users.noreply.github.com>
2023-08-21 22:29:53 +0200

    add quasar

--- resume-creator/src/views/HomeView.vue
+++ resume-creator/src/views/HomeView.vue
@@ -3,8 +3,12 @@
 
 <template>
   <main>
-    <h1>Your Resume<br/>Wizzard</h1>
-    <button>Create for free</button>
+    <h1>Your Resume<br />Wizzard</h1>
+    <a href="/creator" target="_self">
+      <button>
+        Create for free
+      </button>
+    </a>
   </main>
   <footer>
 
@@ -13,34 +17,33 @@
 
 
 <style scoped>
+main {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  gap: 5em;
 
-  main {
-    height:100vh;
-    display:flex;
-    justify-content:center;
-    align-items: center;
-    flex-direction:column;
-    text-align: center;
-    gap: 5em;
-
-    & h1 {
-      font-size: clamp(8.125rem, 5.9821rem + 3.3482vw, 10rem);
-      font-weight: 700;
-    }
-
-    & button {
-      border-radius: 8px;
-      padding: 1.25rem;
-      font-size: clamp(3rem, 1.8571rem + 1.7857vw, 4rem);
-      background-color: var(--primary-bgcolor);
-      color: var(--primary-color);
-      border-width: 8px;
-    }
+  & h1 {
+    font-size: clamp(8.125rem, 5.9821rem + 3.3482vw, 10rem);
+    font-weight: 700;
+    line-height: unset;
   }
 
-  footer {
-    height: 200px;
-    background-color: #2b2b2b;
+  & button {
+    border-radius: 8px;
+    padding: 1.25rem;
+    font-size: clamp(3rem, 1.8571rem + 1.7857vw, 4rem);
+    background-color: var(--primary-bgcolor);
+    color: var(--primary-color);
+    border-width: 8px;
   }
+}
 
+footer {
+  height: 200px;
+  background-color: #2b2b2b;
+}
 </style>
