34f7b28388aea5b2cbc059f774c8b94416dd9b14
Łukasz Cieślak <56347002+xlazure@users.noreply.github.com>
2023-08-21 22:03:32 +0200

    init branch and project

---
+++ resume-creator/src/views/CreatorView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Creator page</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ resume-creator/src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Your Resume<br/>Wizzard</h1>
+    <button>Create for free</button>
+  </main>
+  <footer>
+
+  </footer>
+</template>
+
+
+<style scoped>
+
+  main {
+    height:100vh;
+    display:flex;
+    justify-content:center;
+    align-items: center;
+    flex-direction:column;
+    text-align: center;
+    gap: 5em;
+
+    & h1 {
+      font-size: clamp(8.125rem, 5.9821rem + 3.3482vw, 10rem);
+      font-weight: 700;
+    }
+
+    & button {
+      border-radius: 8px;
+      padding: 1.25rem;
+      font-size: clamp(3rem, 1.8571rem + 1.7857vw, 4rem);
+      background-color: var(--primary-bgcolor);
+      color: var(--primary-color);
+      border-width: 8px;
+    }
+  }
+
+  footer {
+    height: 200px;
+    background-color: #2b2b2b;
+  }
+
+</style>
